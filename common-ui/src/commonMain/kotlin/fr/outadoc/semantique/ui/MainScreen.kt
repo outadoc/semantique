88b96871c22679ed46e37d4542e04e7248b2ff8a
Baptiste Candellier <baptiste@candellier.me>
2022-04-10 14:51:51 +0200

    refactor: split MainScreen.kt

--- common-ui/src/commonMain/kotlin/fr/outadoc/semantique/ui/MainScreen.kt
+++ common-ui/src/commonMain/kotlin/fr/outadoc/semantique/ui/MainScreen.kt
@@ -1,13 +1,5 @@
 package fr.outadoc.semantique.ui
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Help
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -17,21 +9,20 @@ import androidx.compose.ui.platform.LocalUriHandler
 import fr.outadoc.semantique.viewmodels.MainViewModel
 import kotlinx.coroutines.flow.collect
 import org.kodein.di.compose.instance
-import java.util.*
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    val mainViewModel: MainViewModel by instance()
-    val state by mainViewModel.state.collectAsState()
+    val viewModel: MainViewModel by instance()
+    val state by viewModel.state.collectAsState()
 
     val uriHandler = LocalUriHandler.current
 
-    LaunchedEffect(mainViewModel) {
-        mainViewModel.onStart()
+    LaunchedEffect(viewModel) {
+        viewModel.onStart()
     }
 
-    LaunchedEffect(mainViewModel.events) {
-        mainViewModel.events.collect { event ->
+    LaunchedEffect(viewModel.events) {
+        viewModel.events.collect { event ->
             when (event) {
                 is MainViewModel.Event.OpenUri -> {
                     uriHandler.openUri(event.uri)
@@ -40,36 +31,13 @@ fun MainScreen(modifier: Modifier = Modifier) {
         }
     }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                actions = {
-                    IconButton(onClick = mainViewModel::onHelpButtonClicked) {
-                        Icon(
-                            Icons.Default.Help,
-                            contentDescription = "Informations sur le jeu"
-                        )
-                    }
-
-                    LanguagePicker(
-                        currentLanguageCode = state.languageCode,
-                        onLanguageCodeSelected = mainViewModel::switchLanguage,
-                        availableLanguageCodes = listOf(
-                            Locale.FRENCH.language,
-                            Locale.ENGLISH.language
-                        )
-                    )
-                },
-                title = { Text("Sémantique") }
-            )
-        }
-    ) { padding ->
-        MainList(
-            modifier = modifier.padding(padding),
-            state = state,
-            onInputChanged = mainViewModel::onInputChanged,
-            onGuessWordClicked = mainViewModel::onGuessWordClicked,
-            onDisplayNeighborsChanged = mainViewModel::onDisplayNeighborsToggled
-        )
-    }
+    MainScreenContent(
+        modifier = modifier,
+        state = state,
+        onHelpButtonClicked = viewModel::onHelpButtonClicked,
+        switchLanguage = viewModel::switchLanguage,
+        onInputChanged = viewModel::onInputChanged,
+        onGuessWordClicked = viewModel::onGuessWordClicked,
+        onDisplayNeighborsToggled = viewModel::onDisplayNeighborsToggled
+    )
 }
---
+++ common-ui/src/commonMain/kotlin/fr/outadoc/semantique/ui/MainScreenContent.kt
@@ -0,0 +1,58 @@
+package fr.outadoc.semantique.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Help
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import fr.outadoc.semantique.viewmodels.MainViewModel
+import java.util.*
+
+@Composable
+fun MainScreenContent(
+    modifier: Modifier = Modifier,
+    state: MainViewModel.State,
+    onHelpButtonClicked: () -> Unit,
+    switchLanguage: (String) -> Unit,
+    onInputChanged: (String) -> Unit,
+    onGuessWordClicked: () -> Unit,
+    onDisplayNeighborsToggled: (Boolean) -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                actions = {
+                    IconButton(onClick = onHelpButtonClicked) {
+                        Icon(
+                            Icons.Default.Help,
+                            contentDescription = "Informations sur le jeu"
+                        )
+                    }
+
+                    LanguagePicker(
+                        currentLanguageCode = state.languageCode,
+                        onLanguageCodeSelected = switchLanguage,
+                        availableLanguageCodes = listOf(
+                            Locale.FRENCH.language,
+                            Locale.ENGLISH.language
+                        )
+                    )
+                },
+                title = { Text("Sémantique") }
+            )
+        }
+    ) { padding ->
+        MainList(
+            modifier = modifier.padding(padding),
+            state = state,
+            onInputChanged = onInputChanged,
+            onGuessWordClicked = onGuessWordClicked,
+            onDisplayNeighborsChanged = onDisplayNeighborsToggled
+        )
+    }
+}
